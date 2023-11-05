3711812a01af25f83e2ea77faebf46b2d103b4f3
Rengoku Kyōjurō <123724356+Radionbes228@users.noreply.github.com>
2023-11-05 19:56:29 +0300

    Delete src/main/java/com/radionbes/spring/RiverBank/calculation directory

--- src/main/java/com/radionbes/spring/RiverBank/calculation/CalculationCredit.java
+++
@@ -1,7 +0,0 @@
-package com.radionbes.spring.RiverBank.calculation;
-
-public class CalculationCredit {
-    public Double amountPerMonth(Integer loanAmount, Integer duration, Double interestRate){
-        return null;
-    }
-}
